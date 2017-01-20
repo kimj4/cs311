2864d2c16176933c9ec77cc7e2e4976c07bcd764
Ju Yun Kim <kimj4@github.com>
2017-01-20 00:55:45 -0600

    deleted all the stuff in day1

--- day1/h060.sh
+++
@@ -1,1 +0,0 @@
-clang 060mainEffect.c 000pixel.o -lglfw3 -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -ldl -lXinerama -lXcursor -lrt
--- day1/l.sh
+++
@@ -1,1 +0,0 @@
-clang 090mainScene.c 000pixel.o -lglfw3 -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -ldl -lXinerama -lXcursor -lrt
