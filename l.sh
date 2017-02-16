2cf60c6bb07ad9fbce7049337a5fbca1d8f7635b
Ju Yun Kim <kimj4@github.com>
2017-02-15 21:04:09 -0600

    some progress on the first of the GLFW assignments

--- l.sh
+++ l.sh
@@ -1,1 +1,1 @@
-clang 170mainSpecular.c 000pixel.o -lglfw3 -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -ldl -lXinerama -lXcursor -lrt
+clang 530mainScene.c -lglfw3 -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -ldl -lXinerama -lXcursor -lrt
