060eacb7652d81a48bac2e0129912cf20c697278
Ju Yun Kim <kimj4@github.com>
2017-02-19 21:26:56 -0600

    file separation and deletions

--- 080renderer.c
+++
@@ -1,8 +0,0 @@
-typedef struct renRenderer renRenderer;
-
-struct renRenderer {
-	int unifDim; // dimension of stuff properties that apply to the whole triangle
-	int texNum;  // number of textures
-	int attrDim; // dimension of information specific to each vertex
-  int varyDim;
-};
